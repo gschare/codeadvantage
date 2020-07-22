96c2dbc382169a51cb37fb3d58eb765a7572bb64
Gregory <grschare1315@gmail.com>
2020-07-22 12:19:51 -0700

    Bug fixes

--- Afternoon Python/piglatin.py
+++ Afternoon Python/piglatin.py
@@ -1,29 +1,41 @@
+'''
+Basic string operations:
+
 greeting = "hello "
 world = "world"
 
-greeting[0:3] # -> "hel"
-world[3:] # -> "ld"
-greeting + world # -> "hello world"
+greeting[0:3]       # -> "hel"
+world[3:]           # -> "ld"
+greeting + world    # -> "hello world"
 greeting.split("e") # -> ["h", "llo"]
-greeting*3 # -> "hello hello hello "
+greeting*3          # -> "hello hello hello "
+'''
+
+# Convert a sentence to pig latin by the following pattern:
+# hello -> ellohay
+# am -> amay
+# great -> eatgray
 
 sentence = input("Write a sentence: ")
-# Gregory -> Egorygray
-# My name is Gregory -> Ymay Amenay Isay Egorygray
-# to -> otay
-# if -> ifay
-
-# ant -> antay
 
 vowels = ['a', 'e', 'i', 'o', 'u']
 
 sentence = sentence.split(" ")
-print(sentence)
+
 output = []
 for word in sentence:
     if word[0].lower() in vowels:
         output.append(word + "ay")
     else:
-        index = min([word.find(v) for v in vowels if word.find(v)!=-1])
+        vowel_places = [word.find(v) for v in vowels if word.find(v)!=-1]
+        if vowel_places:   # "if vowel_places has stuff in it"
+            index = min(vowel_places)
+        else:
+            index = 1
+        '''
+        A faster way of doing the above:
+        index = min(vowel_places) if vowel_places else 1
+        '''
         output.append(word[index:] + word[:index].lower() + "ay")
-print(output)
+        
+print(" ".join(output))
