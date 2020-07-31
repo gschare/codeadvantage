b5d26342fd39a24dd0219d3d9db586296918d1be
gschare <grschare1315@gmail.com>
2020-07-31 09:33:11 -0700

    Level 3 Day 5

--- Morning Python/racing_with_classes.py
+++ Morning Python/racing_with_classes.py
@@ -1,29 +1,58 @@
 import turtle, random
 
-w, h = 1000, 200
+w, h = 1000, 400
 screen = turtle.Screen()
 screen.setup(width=w, height=h)
+screen.tracer(0)
+framerate = 20
+
+class Field:
+    offset = 20
+    top    =   screen.window_height()/2 - offset
+    bottom = -(screen.window_height()/2 - offset)
+    left   = -(screen.window_width()/2  - offset)
+    right  =   screen.window_width()/2  - offset
+    
+    def __init__(self):
+        self.borders = turtle.Turtle()
+        self.drawBorders()
+
+    def drawBorders(self):
+        self.borders.hideturtle()
+        self.borders.penup()
+        self.borders.goto(self.left, self.top)
+        self.borders.pendown()
+        self.borders.goto(self.right, self.top)
+        self.borders.goto(self.right, self.bottom)
+        self.borders.goto(self.left, self.bottom)
+        self.borders.goto(self.left, self.top)
 
 class RacingTurtle:
     time = 0.1
     isPlayer = False
+    turn_limit = 15
+    min_speed = 0
+    max_speed = 10
+    size = 10
+    starting_x_offset = 20
     
-    def __init__(self, name, color, starting_y):
+    def __init__(self, name, color, starting_y, field):
+        self.field = field
         self.name = name
         self.color = color
         self.turtle = turtle.Turtle(shape="turtle")
         self.turtle.color(color)
         self.turtle.penup()
-        self.turtle.goto(-screen.window_width()/2 + 10, starting_y)
+        self.turtle.goto(self.field.left + self.starting_x_offset, starting_y)
         self.prevx = self.turtle.xcor()
         self.prevy = self.turtle.ycor()
 
     def check_collision(self):
-        if self.turtle.xcor() <= -screen.window_width()/2 + 10:
+        if self.turtle.xcor() - self.size <= self.field.left:
             return True
-        elif self.turtle.ycor() <= -screen.window_height()/2 + 10:
+        elif self.turtle.ycor() - self.size <= self.field.bottom:
             return True
-        elif self.turtle.ycor() >= screen.window_height()/2 - 10:
+        elif self.turtle.ycor() + self.size >= self.field.top:
             return True
         else:
             return False
@@ -31,8 +60,8 @@ class RacingTurtle:
     def move(self):
 
         # if the turtle is a player, don't turn randomly
-        turn_angle = random.randint(-15, 15) * (not self.isPlayer)
-        speed = random.randint(0, 10)
+        turn_angle = random.randint(-self.turn_limit, self.turn_limit) * (not self.isPlayer)
+        speed = random.randint(self.min_speed, self.max_speed)
         self.turtle.forward(speed)
         self.turtle.right(turn_angle)
 
@@ -44,8 +73,8 @@ class RacingTurtle:
                 direction = 1
             else:
                 direction = -1
-            self.turtle.right(60*direction)  # direction is -1 or 1
-            self.turtle.forward(10)
+            self.turtle.right(180)  # direction is -1 or 1
+            self.turtle.forward(20)
 
         self.prevx = self.turtle.xcor()
         self.prevy = self.turtle.ycor()
@@ -55,28 +84,30 @@ class RacingTurtle:
     def checkWin(self):
         # if the turtle has passed the finish line
         # it may be prudent to make the finish line a separate variable
-        if self.turtle.xcor() >= screen.window_width()/2 - 10:
+        if self.turtle.xcor() + self.size >= self.field.right:
             return True
         else:
             return False
 
-def calculateStartingY(num_turtles):
-    first_pos = screen.window_height() / (num_turtles+1)
+def calculateStartingY(num_turtles, field):
+    first_pos = (field.top - field.bottom) / (num_turtles+1)
     positions = []
     for i in range(num_turtles):
-        positions.append(first_pos * (i+1) - (screen.window_height()/2))
+        positions.append(first_pos * (i+1) - ((field.top - field.bottom)/2))
     return positions    
 
-positions = calculateStartingY(5)
+field = Field()
+
+positions = calculateStartingY(5, field)
 
 competitors = [
-    RacingTurtle("Jeff", "pink", positions[0]),
-    RacingTurtle("Bob", "blue", positions[1]),
-    RacingTurtle("Harold", "lime", positions[3]),
-    RacingTurtle("Marilyn", "red", positions[4])
+    RacingTurtle("Jeff", "pink", positions[0], field),
+    RacingTurtle("Bob", "blue", positions[1], field),
+    RacingTurtle("Harold", "lime", positions[3], field),
+    RacingTurtle("Marilyn", "red", positions[4], field)
     ]
 
-player = RacingTurtle("Gregory", "green", positions[2])
+player = RacingTurtle("Gregory", "green", positions[2], field)
 player.isPlayer = True
 
 def goLeft():
@@ -85,21 +116,27 @@ def goLeft():
 def goRight():
     player.turtle.right(10)
 
+screen.onkey(goLeft, key="Left")
+screen.onkey(goRight, key="Right")
+screen.listen()
+
 # soon we will convert this while loop to a framerate loop to keep it consistent
-won = False
-while not won:
+def frame():
     for t in competitors:
         if t.checkWin():
             print(t.name + " wins!")
-            won = True
+            return
         t.move()
         
     if player.checkWin():
         print(player.name + " wins!")
-        won = True
-    screen.onkey(goLeft, key="Left")
-    screen.onkey(goRight, key="Right")
-    screen.listen()
+        return
+    
     player.move()  # we reuse the NPC move method, except we don't turn randomly
+
+    screen.update()
+    screen.ontimer(frame, framerate)
+
+frame()
 
 screen.mainloop()
